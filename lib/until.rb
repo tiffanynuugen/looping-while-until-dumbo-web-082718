49f3ec69b7ce47ec7e6b3d583fa49b5e60b0c75c
Tiffany Nguyen <tiffanynuugen@gmail.com>
2018-08-15 00:14:38 +0000

    automatically backed up by learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,7 +1,7 @@
 def using_until
-        levitation_force = 6
-        until count < 10
-          puts "Wingardium Leviosa"
-          count += 1
-        #your code here
-      end
+  levitation_force = 6
+  until count < 10
+    puts "Wingardium Leviosa"
+    count += 1
+  end
+end
